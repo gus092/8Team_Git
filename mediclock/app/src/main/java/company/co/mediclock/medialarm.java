5254a34b9d137878dc3698778e92719d21d56296
jueun lee <jooe0824@gmail.com>
2020-04-30 16:17:38 +0900

    jueun medi added
    알람 설정 및 아침점심저녁 문자 구분 ui

---
+++ mediclock/app/src/main/java/company/co/mediclock/medialarm.java
@@ -0,0 +1,14 @@
+package company.co.mediclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class medialarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medialarm);
+    }
+}
