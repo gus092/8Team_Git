6175b1b90d4cb9a0a4d016d359b169dd95e1ec91
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-04-30 16:18:01 +0900

    shareactivity

--- mediclock/app/src/main/java/company/co/mediclock/MainActivity.java
+++ mediclock/app/src/main/java/company/co/mediclock/MainActivity.java
@@ -2,7 +2,10 @@ package company.co.mediclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.button4);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        ShareActiviry.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
     }
 }
