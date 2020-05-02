bbe633f9503a5e8928b833727e55fcad6639d553
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-05-02 15:56:54 +0900

    shareactivity layout 2/3

--- mediclock/app/src/main/java/company/co/mediclock/MainActivity.java
+++ mediclock/app/src/main/java/company/co/mediclock/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(
                         getApplicationContext(), // 현재 화면의 제어권자
-                        ShareActiviry.class); // 다음 넘어갈 클래스 지정
+                        ShareActivity.class); // 다음 넘어갈 클래스 지정
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
